3b4416dc27318cb3898ec8f8223e38a717d3255d
tubocurarine <yangyingchao@gmail.com>
2010-06-03 01:23:05 +0000

    Update: Changed username for tramp.
    Author:yangyingchao, 2010-06-03

--- .emacs.d/rc/emacs-rc-tramp.el
+++ .emacs.d/rc/emacs-rc-tramp.el
@@ -1,10 +1,10 @@
 ;;; emacs-rc-tramp.el begins ---
 (require 'tramp)
-(setq tramp-default-user "yyc")
 
 (if (eq system-type 'windows-nt)
-    (setq tramp-default-method "plink")
-  (setq tramp-default-method "ssh"))
+    (setq tramp-default-method "plink" tramp-default-user "itc208024")
+  (setq tramp-default-method "ssh" tramp-default-user "yyc")
+  )
 
 (nconc  (cadr (assq 'tramp-login-args (assoc "ssh" tramp-methods))) '("/bin/sh" "-i"))
 (setcdr       (assq 'tramp-remote-sh  (assoc "ssh" tramp-methods))  '("/bin/sh -i"))
@@ -13,8 +13,8 @@
 (setq tramp-auto-save-directory "~/.emacs.d/auto-save-list")
 
 (tramp-set-completion-function "ssh"
-			       '((tramp-parse-sconfig "/etc/ssh_config")
-				 (tramp-parse-sconfig "~/.ssh/config")))
+                   '((tramp-parse-sconfig "/etc/ssh_config")
+                 (tramp-parse-sconfig "~/.ssh/config")))
 
 (provide 'emacs-rc-tramp)
 ;;; emacs-rc-tramp.el ends here
