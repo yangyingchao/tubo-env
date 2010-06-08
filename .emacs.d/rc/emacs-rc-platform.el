987fc26b42a36c2f7c3ebcc95f8b4d952fe68859
tubocurarine <yangyingchao@gmail.com>
2010-06-08 04:17:39 +0000

    Update: Added new init file.
    Author:yangyingchao, 2010-06-08

---
+++ .emacs.d/rc/emacs-rc-platform.el
@@ -0,0 +1,60 @@
+;;;; -*- emacs-lisp -*- -*- coding: utf-8; -*-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; $Id: emacs-rc-platform.el, 星期二, 六月  8 2010
+
+(defvar shellpath nil
+  "Path of which shell will be used by emacs.")
+(defvar cedet-sys-include-dirs nil "" )
+
+(defvar platform-rfc-dir nil)
+
+(defun setup-windows ()
+  "Setup variables for windows."
+  (set-frame-font
+   " -outline-Monaco-normal-normal-normal-mono-14-*-*-*-c-*-iso8859-1")
+  (require 'cygwin-mount)
+  (cygwin-mount-activate)
+
+  (setq shellpath "d:/cygwin/bin/bash.exe")
+  (setq cedet-sys-include-dirs (list
+                                "d:/gnu/lib/gcc/mingw32/3.4.5/include"
+                                "d:/gnu/include/c++/4.1.1"
+                                "d:/gnu/include"
+                                "d:/gnu/include/gtk-2.0"
+                                "d:/gnu/include/gnu"
+                                "d:/gnu/include/gtk-2.0/gtk"
+                                "d:/gnu/include/gtk-2.0/gdk-pixbuf"
+                                "d:/gnu/include/gtk-2.0/gtk"
+                                "c:/Program Files/Microsoft Visual Studio 9.0/VC/include"
+                                ))
+  (setq  platform-rfc-dir "e:/Techbooks/rfcs/")
+  )
+
+(defun setup-linux ()
+  "Setup variables for linux"
+  (setq shellpath "/bin/bash")
+  (setq cedet-sys-include-dirs (list
+                                "/usr/include"
+                                "/usr/include/bits"
+                                "/usr/include/glib-2.0"
+                                "/usr/include/gnu"
+                                "/usr/include/gtk-2.0"
+                                "/usr/include/gtk-2.0/gdk-pixbuf"
+                                "/usr/include/gtk-2.0/gtk"
+                                "/usr/local/include"
+                                "/usr/local/include"
+                                ))
+  (setq  platform-rfc-dir "~/Documents/rfcs/")
+  )
+
+(if (eq system-type 'windows-nt)
+    (setup-windows)
+  (setup-linux)
+  )
+
+(setq shell-file-name shellpath)
+(setq-default explicit-shell-file-name shellpath)
+(setenv "SHELL" shell-file-name)
+
+(provide 'emacs-rc-platform)
+;;;;; emacs-rc-platform.el ends here
