8090f65d429ffec133f09a98d3c7b4406f2d7b43
tubocurarine <yangyingchao@gmail.com>
2010-06-18 05:53:36 +0000

    Update: Modified ac-sources in different modes.
    Author:yangyingchao, 2010-06-18

--- .emacs.d/rc/emacs-rc-auto-complete.el
+++ .emacs.d/rc/emacs-rc-auto-complete.el
@@ -1,35 +1,10 @@
+;;;; -*- emacs-lisp -*- -*- coding: utf-8; -*-
 ;;; emacs-rc-auto-complete.el ---
-
-;; Generic setup.
 (require 'auto-complete)
 (require 'auto-complete-config)
-(ac-config-default)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/dict")
-(global-auto-complete-mode t)           ;enable global-mode
 
-(setq ac-auto-start t)
-(setq ac-dwim t)                        ;Do what i mean
-(setq ac-override-local-map nil)        ;don't override local map
-(setq ac-modes '(ada-mode asm-mode c++-mode c-mode cc-mode cperl-mode css-mode
-                          ecmascript-mode emacs-lisp-mode emms-tag-editor-mode f90-mode
-                          fortran-mode haskell-mode java-mode javascript-mode
-                          latex-mode lisp-interaction-mode lisp-mode
-                          literate-haskell-mode makefile-mode org-mode perl-mode
-                          php-mode python-mode ruby-mode scheme-mode sgml-mode sh-mode
-                          text-mode xml-mode
-                          ))
-
-;;; Lisp mode
-(dolist (hook (list
-               'emacs-lisp-mode-hook
-               'lisp-interaction-mode
-               ))
-  (add-hook hook '(lambda ()
-                    (add-to-list 'ac-sources 'ac-source-symbols))))
-;;;; start completion when entered 3 characters
-(setq ac-auto-start 3)
-(define-key ac-complete-mode-map "\t" 'ac-complete)
-(define-key ac-complete-mode-map "\r" nil)
+(ac-config-default) ; 调用默认设置, defined in auto-complete-config.el。
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/templates/dic")
 
 ;;; Autofill Keybinding.
 (when (require 'auto-complete nil t)
@@ -37,31 +12,49 @@
   (set-face-background 'ac-selection-face "steelblue")
   (set-face-background 'ac-candidate-face "lightgray")
   (set-face-underline-p 'ac-candidate-face "darkgray")
-  (define-key ac-complete-mode-map "\t" 'ac-expand)
+  (define-key ac-complete-mode-map (kbd "<C-tab>") 'ac-expand)
   (define-key ac-complete-mode-map "\M-\r" 'ac-complete)
   (define-key ac-complete-mode-map [(tab)] 'ac-complete)
   (define-key ac-complete-mode-map "\M-n" 'ac-next)
   (define-key ac-complete-mode-map "\M-p" 'ac-previous)
-  (setq ac-auto-start 2)
+  (setq ac-auto-start 3)
   (setq ac-dwim t)
-  (setq ac-modes
-        (append ac-modes
-                '(eshell-mode))
-        )
+  (setq ac-override-local-map nil)  ;don't override local map
+  (setq ac-modes '(
+                   ada-mode
+                   asm-mode c++-mode c-mode cc-mode cperl-mode css-mode
+                   ecmascript-mode emacs-lisp-mode emms-tag-editor-mode f90-mode
+                   fortran-mode haskell-mode java-mode javascript-mode
+                   latex-mode lisp-interaction-mode lisp-mode
+                   literate-haskell-mode makefile-mode org-mode perl-mode
+                   php-mode python-mode ruby-mode scheme-mode sgml-mode sh-mode
+                   text-mode xml-mode  eshell-mode
+                   ))
   )
 
-;; The sources for common all mode.
-(defun yyc/common-ac-source-setup ()
-  (setq ac-sources
-        '(
-          ac-source-yasnippet
-          ac-source-gtags
-          ac-source-semantic
-          ac-source-words-in-buffer
-          ac-source-filename
-          )))
-(add-hook 'auto-complete-mode-hook 'yyc/common-ac-source-setup)
 
+;; The sources for common all mode.
+(setq-default ac-sources
+      '(
+        ac-source-yasnippet
+        ac-source-words-in-buffer
+        ac-source-filename
+        ))
+
+;;; Lisp mode
+(defun yyc/ac-source-lisp ()
+  "Sources for lisp mode"
+  (setq ac-sources (append '(ac-source-symbols) ac-sources))
+  )
+
+(defun yyc/ac-source-python ()
+  "sources for python mode"
+  (setq ac-sources (append '(ac-source-semantic) ac-sources))
+  )
+
+(add-hook 'emacs-lisp-mode-hook 'yyc/ac-source-lisp)
+(add-hook 'python-mode-hook 'yyc/ac-source-python)
+(global-auto-complete-mode t) ;enable global-mode
 
 (provide 'emacs-rc-auto-complete)
 ;;; emacs-rc-auto-complete.el ends here
