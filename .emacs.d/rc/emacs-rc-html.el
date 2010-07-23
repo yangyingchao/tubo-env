9b1b652e5d566d80c411c83892afd0adab637d40
tubocurarine <yangyingchao@gmail.com>
2010-07-23 02:03:25 +0000

    added new snippets

--- .emacs.d/rc/emacs-rc-html.el
+++ .emacs.d/rc/emacs-rc-html.el
@@ -1,16 +1,24 @@
 ;;;; -*- emacs-lisp -*- -*- coding: utf-8; -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; $Id: emacs-rc-html.el, 星期五, 五月 28 2010
-(defun my-newline ()
+(defun yyc/html-newline ()
   "New line, add <br> into the end of line."
   (interactive)
   (insert "<br>")
   (newline-and-indent)
   )
 
+
+(defun yyc/remove-hrefs ()
+  "Funtion to remove hyperlinks quickly"
+  (interactive)
+  (while (re-search-forward "<a href=.*?\">\\(.*?>\\)</a>" nil t)
+    (replace-match (match-string 1) nil nil))
+  )
+
 (defun my-html-mode-hooks ()
   "description"
-  (local-set-key (kbd "<C-return>") 'my-newline)
+  (local-set-key (kbd "<C-return>") 'yyc/html-newline)
   )
 
 (add-hook 'html-mode-hook 'my-html-mode-hooks)
