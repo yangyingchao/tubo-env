9e833e385c69d62f1f77c8ae2d4eb359876e4e69
tubocurarine <yangyingchao@gmail.com>
2010-08-05 09:57:26 +0000

    None

--- .emacs.d/rc/emacs-rc-functions.el
+++ .emacs.d/rc/emacs-rc-functions.el
@@ -44,6 +44,36 @@
 
 
 
+(defun lazy-set-key (key-alist &optional keymap key-prefix)
+  "This function is to little type when define key binding.
+`KEYMAP' is a add keymap for some binding, default is `current-global-map'.
+`KEY-ALIST' is a alist contain main-key and command.
+`KEY-PREFIX' is a add prefix for some binding, default is nil."
+  (let (key def)
+    (or keymap (setq keymap (current-global-map)))
+    (if key-prefix
+        (setq key-prefix (concat key-prefix " "))
+      (setq key-prefix ""))
+    (dolist (element key-alist)
+      (setq key (car element))
+      (setq def (cdr element))
+      (cond ((stringp key) (setq key (read-kbd-macro (concat key-prefix key))))
+            ((vectorp key) nil)
+            (t (signal 'wrong-type-argument (list 'array key))))
+      (define-key keymap key def))))
+
+(defun lazy-unset-key (key-list &optional keymap)
+  "This function is to little type when unset key binding.
+`KEYMAP' is add keymap for some binding, default is `current-global-map'
+`KEY-LIST' is list contain key."
+  (let (key)
+    (or keymap (setq keymap (current-global-map)))
+    (dolist (key key-list)
+      (cond ((stringp key) (setq key (read-kbd-macro (concat key))))
+            ((vectorp key) nil)
+            (t (signal 'wrong-type-argument (list 'array key))))
+      (define-key keymap key nil))))
+
 ;;;; dos-unix
 (defun dos-unix ()
   (interactive)
--- .emacs.d/rc/emacs-rc-keybindings.el
+++ .emacs.d/rc/emacs-rc-keybindings.el
@@ -1,5 +1,8 @@
 ;;;; -*- emacs-lisp -*- -*- coding: utf-8; -*-
 ;;; emacs-rc-keybindings.el begins ---
+
+(require 'emacs-rc-functions)
+
 ;; (global-set-key [(control f1)] 'open-mylist)
 ;; (global-set-key [(f1)] (lambda()        ;;设定F1为woman快捷键
 ;;           (interactive)
@@ -8,11 +11,14 @@
 (global-set-key [f2] 'zl-newline-up)
 (require 'artist)
 (global-set-key (kbd "<C-S-f2>") 'artist-mode)
-;; (global-set-key [(control f3)] 'highlight-symbol-at-point)
-;; (global-set-key [f3] 'highlight-symbol-next)
-;; (global-set-key [(shift f3)] 'highlight-symbol-remove-all)
-;; (global-set-key [(meta f3)] 'highlight-symbol-prev)
-;; (global-set-key [(control meta f3)] 'highlight-symbol-query-replace)
+
+(require 'highlight-utility)
+(global-set-key [(control f3)] 'highlight-symbol-at-point)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-remove-all)
+(global-set-key [(meta f3)] 'highlight-symbol-prev)
+(global-set-key [(control meta f3)] 'highlight-symbol-query-replace)
+
 (global-set-key [f4] 'goto-line)
 (global-set-key (kbd "<C-f4>") 'kmacro-start-macro-or-insert-counter)
 (global-set-key (kbd "<S-f4>") ' kmacro-end-or-call-macro)
@@ -62,6 +68,54 @@
 (global-set-key "\C-cc" 'comment-region)
 (global-set-key "\C-cu" 'uncomment-region)
 
+
+;;; ### Doc-view ###
+;;; --- 文档阅读器
+(lazy-unset-key
+ '(".")
+ doc-view-mode-map)                     ;卸载按键
+(lazy-set-key
+ '(
+   ("C-M-j" . doc-view-scroll-up-or-next-page+)       ;翻另一个窗口中图书的下一页
+   ("C-M-k" . doc-view-scroll-down-or-previous-page+) ;翻另一个窗口中图书的上一页
+   ))
+(lazy-unset-key
+ '("x" "M-<" "M->")
+ doc-view-mode-map)                     ;卸载一些按键
+(lazy-set-key
+ '(
+   ([remap scroll-up] . doc-view-next-line-or-next-page) ;重新定向按键, 支持 auto-scroll
+   )
+ doc-view-mode-map
+ )
+(lazy-set-key
+ '(
+   ("N" . doc-view-next-page)                      ;下一页
+   ("P" . doc-view-previous-page)                  ;上一页
+   ("." . doc-view-first-page)                     ;第一页
+   ("," . doc-view-last-page)                      ;最后一页
+   ("g" . doc-view-goto-page)                      ;跳到第几页
+   ("e" . doc-view-scroll-down-or-previous-page)   ;向上滚动一屏
+   ("SPC" . doc-view-scroll-up-or-next-page)       ;向下滚动一屏
+   ("j" . doc-view-next-line-or-next-page)         ;下一行或下一屏
+   ("k" . doc-view-previous-line-or-previous-page) ;上一行或上一屏
+   ("t" . doc-view-show-tooltip)                   ;当前页提示
+   ("q" . bury-buffer)                             ;隐藏buffer
+   ("Q" . doc-view-kill-proc-and-buffer)           ;退出并结束进程
+   ("C-s" . doc-view-search)                       ;搜索
+   ("C-S-n" . doc-view-search-next-match)          ;下一个匹配
+   ("C-S-p" . doc-view-search-previous-match)      ;上一个匹配
+   ("+" . doc-view-enlarge)                        ;放大页面
+   ("-" . doc-view-shrink)                         ;缩小页面
+   ("C-c C-c" . doc-view-toggle-display)           ;在文本和图像间切换
+   ("C-c C-t" . doc-view-open-text)                ;打开文本
+   ("r" . revert-buffer)                           ;刷新
+   ("s" . auto-scroll-mode)                        ;自动滚屏
+   ("<" . auto-scroll-faster)                      ;加快滚屏速度
+   (">" . auto-scroll-slower)                      ;减慢滚屏速度
+   )
+ doc-view-mode-map
+ )
 
 (provide 'emacs-rc-keybindings)
 ;;; emacs-rc-keybindings.el ends here
