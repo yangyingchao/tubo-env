5c77a0e09cc818359d20c70bdd57f7ef0600ab20
tubocurarine <yangyingchao@gmail.com>
2010-08-30 11:21:49 +0000

    Deleted snipptes

--- .emacs.d/lisps/yasnippet/snippets/text-mode/latex-mode/rename.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import os
-
-if __name__ == '__main__':
-    for file in os.listdir("."):
-        if file.endswith("x"):
-            os.rename(file, file[:-1])
-            print "Finished: %s"%file
