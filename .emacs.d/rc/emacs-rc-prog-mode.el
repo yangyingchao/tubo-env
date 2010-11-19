c918251b2e4335b774ef02b98f3ae0d16bb6ee68
tubocurarine <yangyingchao@gmail.com>
2010-11-19 01:30:14 +0000

    Use cedet to complete function name for yyc/cflow-function.

--- .emacs.d/rc/emacs-rc-prog-mode.el
+++ .emacs.d/rc/emacs-rc-prog-mode.el
@@ -489,7 +489,9 @@
 
 (defun yyc/cflow-function (function-name)
   "Get call graph of inputed function. "
-  (interactive "sFunction name:\n")
+  ;(interactive "sFunction name:\n")
+  (interactive (list (car (senator-jump-interactive "Function name: "
+                                                    nil nil nil))))
   (setq cmd (format "cflow  -b --main=%s %s" function-name buffer-file-name))
   (setq cflow-buf-name (format "**cflow-%s:%s**"
                                (file-name-nondirectory buffer-file-name)
