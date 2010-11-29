fb1d74586a53f2ee601fb174f5bd4684616ec758
tubocurarine <yangyingchao@gmail.com>
2010-11-29 13:57:57 +0000

    Use let * instead of setq.

--- .emacs.d/rc/emacs-rc-prog-mode.el
+++ .emacs.d/rc/emacs-rc-prog-mode.el
@@ -469,27 +469,23 @@
   )
 
 (require 'cflow-mode)
-(defvar cmd nil nil)
-(defvar cflow-buf nil nil)
-(defvar cflow-buf-name nil nil)
 
 (defun yyc/cflow-function (function-name)
   "Get call graph of inputed function. "
   (interactive (list (car (senator-jump-interactive "Function name: "
                                                     nil nil nil))))
-  (setq cmd (format "cflow  -b --main=%s %s" function-name buffer-file-name))
-  (setq cflow-buf-name (format "**cflow-%s:%s**"
-                               (file-name-nondirectory buffer-file-name)
-                               function-name))
-  (setq cflow-buf (get-buffer-create cflow-buf-name))
-  (set-buffer cflow-buf)
-  (setq buffer-read-only nil)
-  (erase-buffer)
-  (insert (shell-command-to-string cmd))
-  (pop-to-buffer cflow-buf)
-  (goto-char (point-min))
-  (cflow-mode)
-  )
+  (let* ((cmd (format "cflow  -b --main=%s %s" function-name buffer-file-name))
+        (cflow-buf-name (format "**cflow-%s:%s**"
+                                (file-name-nondirectory buffer-file-name)
+                                function-name))
+        (cflow-buf (get-buffer-create cflow-buf-name)))
+    (set-buffer cflow-buf)
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (insert (shell-command-to-string cmd))
+    (pop-to-buffer cflow-buf)
+    (goto-char (point-min))
+    (cflow-mode)))
 
 
 (defun yyc/show-prog-keywords ()
