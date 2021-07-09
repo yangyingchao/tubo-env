b10866cf340d344c4a9605c23dca5fc71c51fd0d
yangyingchao <yangyingchao@gmail.com>
2021-07-09 10:23:34 +0800

    up

--- .local/bin/dislike-wallpaper.sh
+++ .local/bin/dislike-wallpaper.sh
@@ -6,9 +6,43 @@ dev=`swaymsg -r -t get_outputs | jq '. | reverse | to_entries | .[] | select(.va
 file=`cat /tmp/CURRENT_WALLPAPERS | jq ". | to_entries | .[] | select(.value.dev == ${dev} ) |.value.file" |\
   sed 's/"//g'`
 
-if [ -f ${file} ]; then
-    rm -rf ${file}
-fi
+echo "CURRENT:"
+cat /tmp/CURRENT_WALLPAPERS
 
-# download and set up.
-~/.local/bin/download_wallpaper.sh
+echo ""
+echo "DEV: ${dev}"
+
+echo ""
+echo "Going to delete file: ${file}, continue?"
+read ans
+
+case "${ans}" in
+  y|Y)
+    if [ -f ${file} ]; then
+      rm -rf ${file}
+      echo "File ${file} deleted."
+    else
+      echo "File ${file} does not exist."
+    fi
+  ;;
+  *)
+    echo "File ${file} is not deleted"
+  ;;
+esac
+
+echo ""
+echo "Download new wallpaper from wallhaven?"
+
+
+read ans
+
+case "${ans}" in
+  y|Y)
+      ~/.local/bin/download_wallpaper.sh
+  ;;
+  *)
+    echo "Setting wallpaper randomly..."
+    ~/.config/sway/scripts/setup-display.sh Wallpaper
+    echo "done"
+  ;;
+esac
